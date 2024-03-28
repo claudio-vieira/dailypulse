76c0928ade5fa836e8169d91a7614d6d2bf31be9
claudio-vieira <vieirajrclaudio@gmail.com>
2024-03-28 17:22:17 -0300

    Added navigation to IOS and Android

--- androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
@@ -5,13 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.dailypulse.Platform
-import com.example.dailypulse.android.screens.AboutScreen
-import com.example.dailypulse.android.screens.ArticlesScreen
 import com.example.dailypulse.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
@@ -27,20 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticlesScreen(
-                        onAboutButtonClick = { /*TODO*/ },
-                        articlesViewModel = articlesViewModel
-                    )
+                    AppScaffold(articlesViewModel = articlesViewModel)
                 }
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        AboutScreen()
     }
 }
