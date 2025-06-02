7d8f05c811b50d60dd71710080020803b2221f7f
claudio-vieira <vieirajrclaudio@gmail.com>
2025-06-02 19:56:00 -0300

    adjusted about screen design, updated gradle

--- androidApp/src/main/java/com/example/dailypulse/android/screens/AboutScreen.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/screens/AboutScreen.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -88,5 +88,5 @@ private fun RowView(
             style = MaterialTheme.typography.bodyLarge
         )
     }
-    Divider()
+    HorizontalDivider()
 }
